f869ffff3a311045ef485e2e58d9797851c82bcd
M1K <mikepress88@gmail.com>
2020-06-04 13:49:43 +0100

    Proof of Concept working with Chromium

--- enveldt/src/main.ts
+++ enveldt/src/main.ts
@@ -2,6 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
+import * as Go from "./wasm_exec"
+
+new Go.Polyfill();
+
+
+const go = new Go.Go();
+
+WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
+  go.run(result.instance);
+});
 
 Vue.config.productionTip = false;
 
